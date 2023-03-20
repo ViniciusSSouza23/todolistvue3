38ed9b9b30b4462040956e09251db78e615691b5
Vinicius Souza <vinicius.s.souza1996@gmail.com>
2023-03-20 19:39:01 -0300

    :construction: init footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer-component bg-dark">
+    <div class="container text-center py-4">
+      <h6 class="mb-0">
+        Desenvolvido por
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/vinicius-santos-souza/"
+          >Vinicius Souza</a
+        >
+      </h6>
+    </div>
+  </footer>
+</template>
