178eb2fbfa8cccb3cd04217a85c34cd1843e9e74
Vinicius Souza <vinicius.s.souza1996@gmail.com>
2023-03-21 03:13:50 -0300

    add confirmation modal

---
+++ src/components/ConfirmationModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="confirmation-modal p-3">
+    <h5 class="text-center text-white mb-5">{{ copy }}</h5>
+    <div class="d-flex align-items-center justify-content-center flex-column flex-lg-row">
+      <button class="btn btn-secondary me-4" @click="cancel">Cancelar</button>
+      <button class="btn btn-danger" @click="confirm">Confirmar</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    copy: {
+      type: String,
+      default: "Tem certeza?",
+    },
+  },
+  methods: {
+    confirm() {
+      this.$emit("confirm");
+    },
+    cancel() {
+      this.$emit("cancel");
+    },
+  },
+};
+</script>
